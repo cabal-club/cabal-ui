b8433eb3d1278dd13ae671271865c5d58dd9bb69
Nick Warner <nickwarner@gmail.com>
2020-02-20 22:05:48 -0500

    Package updates and starting to use styled-components

--- react/src/components/Avatar.js
+++ react/src/components/Avatar.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Identicon from 'react-blockies'
+import styled from 'styled-components'
 
-import './Avatar.scss'
+const AvatarContainer = styled.div`
+  .identicon {
+    overflow: hidden;
+    border-radius: 10rem;
+  }
+`
 
 export default function Avatar ({ name, bgColor, onClick }) {
   return (
-    <Identicon
-      seed={name}
-      bgColor={bgColor || '#fff'}
-      onClick={onClick}
-    />
+    <AvatarContainer>
+      <Identicon
+        seed={name}
+        bgColor={bgColor || '#fff'}
+        onClick={onClick}
+      />
+    </AvatarContainer>
   )
 }
 
--- react/src/components/Message.js
+++ react/src/components/Message.js
@@ -1,10 +1,58 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import moment from 'moment'
+import styled from 'styled-components'
 
 import Avatar from './Avatar'
 
-import './Message.scss'
+const MessageContainer = styled.div`
+  display: flex;
+  margin-bottom: 0.6rem;
+  padding-top: 0.3rem;
+  width: 100%;
+
+  .date {
+    color: rgba(0, 0, 0, 0.33);
+    font-size: 0.75rem;
+    font-weight: 500;
+    margin-left: 0.5rem;
+  
+    & .date--full {
+      margin-left: 0.5rem;
+      opacity: 0;
+      transition: opacity .2s ease-in-out;
+    }
+  }
+`
+
+const AvatarContainer = styled.div`
+  display: flex;
+  padding: 0 0.7rem 0 1rem;
+`
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 1.5rem 0 0;
+  font-size: 0.875rem;
+  line-height: 1.5;
+
+  &:hover .date .date--full {
+    opacity: 1;
+  }
+`
+
+const Text = styled.div`
+  margin-left: ${props => props.indent ? '32px;' : 'inherit'};
+  margin-top: ${props => props.indent ? '-12px;' : 'inherit'};
+`
+
+const Name = styled.div`
+  font-weight: 700;
+  margin-top: -5px;
+  display: flex;
+  align-items: flex-end;
+`
 
 export default function Message ({ message: { nickname, content, time }, repeatedNickname }) {
   time = moment(time)
@@ -24,17 +72,17 @@ export default function Message ({ message: { nickname, content, time }, repeate
   }
 
   return (
-    <div className='message'>
-      <div className='avatarContainer'>
+    <MessageContainer>
+      <AvatarContainer>
         {repeatedNickname ? null : <Avatar name={nickname || 'conspirator'} />}
-      </div>
-      <div className='content'>
-        {repeatedNickname ? null : <div className='name'>{nickname || 'conspirator'}{renderDate(time)}</div>}
-        <div className={repeatedNickname ? 'text indent' : 'text'}>
+      </AvatarContainer>
+      <Content>
+        {repeatedNickname ? null : <Name>{nickname || 'conspirator'}{renderDate(time)}</Name>}
+        <Text indent={!!repeatedNickname}>
           {enrichText(content)}
-        </div>
-      </div>
-    </div>
+        </Text>
+      </Content>
+    </MessageContainer>
   )
 }
 
